5cda3e56e1317af3b8026b863029dcb636350a14
Josephine Geoghegan <josephineanngeoghegan@gmail.com>
2019-10-22 12:10:41 +0200

    dd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
 //= require_tree .
+
+// function greetUser(name) {
+//   alert('hello' + name);
+// }
+
+// greetUser('Oslo!!!');
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'about/pages'
+  get 'curriculum_vitae/home'
+  root to: 'pages#home'
+  resources :boards
 end
